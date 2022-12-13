e6d48c7b6cdbc87150048fac2c919ddf8ed08972
cpowis15 <connorpowis@ten10.com>
2022-12-13 15:03:38 +0000

    created the test for creating account ( I hope)

---
+++ src/test/java/Tests/SignInDefs.java
@@ -0,0 +1,70 @@
+package Tests;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import pageObjects.BasePage;
+import pageObjects.HomePage;
+
+import java.util.Map;
+
+public class SignInDefs {
+    DriverFactory driverFactory = new DriverFactory();
+    public WebDriver driver = driverFactory.getDriver();
+    private HomePage homepage = new HomePage(driver);
+    private BasePage basePage = new BasePage(driver);
+
+    @Given("I have navigated to the create an account page")
+        public void IHaveNavigatedToTheCreateAnAccountPage(){
+            homepage.goTo();
+        homepage.navigateToSignInPage();
+
+
+        }
+
+    @When("I enter valid details")
+    public void iEnterValidDetails(DataTable dataTable) {
+        Map<String, String> data = dataTable.asMap(String.class, String.class);
+        String FirstName = data.get("First Name");
+        driver.findElement(By.cssSelector("input[name=\"firstname\"]")).sendKeys(FirstName);
+        String LastName = data.get("Last Name");
+        driver.findElement(By.cssSelector("input[name=\"lastname\"]")).sendKeys(LastName);
+        String Email = data.get("Email");
+        driver.findElement(By.cssSelector("input[name=\"email\"]")).sendKeys(Email);
+        String Password = data.get("Password");
+        driver.findElement(By.cssSelector("input[name=\"password\"]")).sendKeys(Password);
+        String Birthday = data.get("Birthdate");
+        driver.findElement(By.cssSelector("input[name=\"birthday\"]")).sendKeys(Birthday);
+    }
+
+
+    @And("Hit Save")
+    public void hitSave() {
+        basePage.waitAndClick(By.cssSelector("button[type=\"submit\"]"));
+    }
+
+    @Then("An account is created and I am logged in")
+    public void anAccountIsCreatedAndIAmLoggedIn() {
+    }
+
+    @Given("I have navigated to the login page")
+    public void iHaveNavigatedToTheLoginPage() {
+    }
+
+    @When("I input valid details")
+    public void iInputValidDetails() {
+    }
+
+    @And("Hit Enter")
+    public void hitEnter() {
+    }
+
+    @Then("I am logged in successfully")
+    public void iAmLoggedInSuccessfully() {
+    }
+}
+}
